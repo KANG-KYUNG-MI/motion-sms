9aeba020554bf1a1731c3897c25858f504fad388
Kyung <kyung.mi.kang@gritacademy.se>
2023-09-08 14:26:58 +0200

    typescript

--- src/app.ts
+++ src/app.ts
@@ -38,6 +38,16 @@ class App {
             TextSectionInput,
             (input: TextSectionInput) => new TodoComponent(input.title, input.body));
 
+            
+    // For demo :)
+    this.page.addChild(new ImageComponent('Image Title', 'https://picsum.photos/8 00/400'));
+    this.page.addChild(new VideoComponent('Video Title', 'https://youtu.be/D7cwvvA7cP0'));
+    this.page.addChild(new NoteComponent('Note Title', "Don't forget to code your dream"));
+    this.page.addChild(new TodoComponent('Todo Title', 'TypeScript Course!'));
+    this.page.addChild(new ImageComponent('Image Title', 'https://picsum.photos/800/400'));
+    this.page.addChild(new VideoComponent('Video Title', 'https://youtu.be/D7cwvvA7cP0'));
+    this.page.addChild(new NoteComponent('Note Title', "Don't forget to code your dream"));
+    this.page.addChild(new TodoComponent('Todo Title', 'TypeScript Course!'));
 
         //this.element = ul
         //New PageComponent().attachTo(main) =  main.insertAdjacentElement(position, ul);
@@ -155,8 +165,9 @@ class App {
         //             dialog.removeFrom(dialogRoot);
         //         });
         //     });
+
     }
-    //---------------------------------------------------------------
+
 
 
     private bindElementToDialog<T extends (MediaData | TextData) & Component>(
--- src/componenet/dialog/dialog.ts
+++ src/componenet/dialog/dialog.ts
@@ -25,7 +25,7 @@ export class InputDialog extends BaseComponent<HTMLElement> implements Composabl
     constructor() {
         super(`<dialog class="dialog">
                <div class= "dialog_container">
-               <button class="close">*</button>
+               <button class="close">Cancek</button>
                <div id="dialog_body">
                
                </div>
